44c1ae18cae911e3507c2b1ce8a995a97784b7c8
S403 <S403@S403-PC>
2016-07-05 10:25:05 +0900

    commit

---
+++ src/chapter01/NHelloWorld01.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class NHelloWorld01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		
+		System.out.println(count);
+	}
+}
