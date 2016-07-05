a8b3049e978053f15885e0ecefa081ef5e3f364d
S403 <S403@S403-PC>
2016-07-05 10:34:18 +0900

    asdasdas

--- src/chapter01/MonthDays.java
+++ src/chapter01/MonthDays.java
@@ -15,7 +15,7 @@ public class MonthDays {
 	public static void ifMonthDays(int month) {
 		int days = 0;
 
-		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 12) {
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
 			days = 31;
 		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
 			days = 30;
